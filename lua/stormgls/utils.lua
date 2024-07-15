9296cbbf7f3e8f41e1f16afa0994ea2f49ec08a6
rakuyo <james.gross@vertex.link>
2024-07-15 13:54:44 -0400

    working on plugin install

---
+++ lua/stormgls/utils.lua
@@ -0,0 +1,27 @@
+local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig.configs'
+local util = require 'lspconfig.util'
+
+local dir = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h")
+
+local M = {}
+
+configs.storm = {
+  default_config = {
+    cmd = { "python", dir .. "/../../stormgls/stormgls.py" },
+    filetypes = {'storm'},
+    autostart = true,
+    single_file_support = true,
+    root_dir = util.find_git_ancestor,
+    settings = {},
+  },
+}
+
+lspconfig.storm.setup{}
+
+vim.api.nvim_create_user_command('StormUpdate', function()
+    local job = vim.fn.jobstart('pip install -r ' .. dir .. '/../../requirements.txt')
+    --, { cwd = '/path/to/working/dir', on_exit = some_function, on_stdout = some_other_function, on_stderr = some_third_function } ) 
+end, {})
+
+return M
---
+++ plugin/stormgls.lua
@@ -0,0 +1,5 @@
+if vim.g.loaded_storm then
+  return
+end
+vim.g.loaded_storm = true
+local api = require('stormgls.utils')
